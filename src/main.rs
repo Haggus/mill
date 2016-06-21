d8390775a6eb79882cbb1dd31fe527710e0b079e
Mateusz Mrowiec <mateusz@kotrak.pl>
2016-06-21 12:43:00 +0200

    Set program params and help using clap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+extern crate clap;
 extern crate notify;
 
 use std::sync::mpsc::channel;
+use clap::{Arg, App, SubCommand};
 use notify::{RecommendedWatcher, Watcher};
 
 fn watch() -> notify::Result<()> {
@@ -28,7 +30,31 @@ fn watch() -> notify::Result<()> {
 }
 
 fn main() {
-    if let Err(err) = watch() {
-        println!("Error! {:?}", err)
+    let matches = App::new("mill")
+        .version("0.1")
+        .author("Mateusz Mrowiec <matt.mrowiec@gmail.com>")
+        .about("Syncs two folders")
+        .arg(Arg::with_name("clean")
+             .short("c")
+             .long("clean")
+             .value_name("FOLDER")
+             .takes_value(true)
+             .help("Sets the clean folder"))
+        .arg(Arg::with_name("dirty")
+             .short("d")
+             .long("dirty")
+             .value_name("FOLDER")
+             .takes_value(true)
+             .help("Sets the dirty folder"))
+        .subcommand(SubCommand::with_name("watch")
+                    .about("Watches folders for changes"))
+        .get_matches();
+
+    println!("{:?}", matches);
+
+    if matches.is_present("watch") {
+        if let Err(err) = watch() {
+            println!("Error! {:?}", err)
+        }
     }
 }
