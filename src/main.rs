5d1e69204f7cc9286840816cfff4f82a16d185ef
Mateusz Mrowiec <mateusz@kotrak.pl>
2016-06-21 12:27:07 +0200

    Rsnotify example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate notify;
+
+use std::sync::mpsc::channel;
+use notify::{RecommendedWatcher, Watcher};
+
+fn watch() -> notify::Result<()> {
+    //create channel to get events
+    let (tx, rx) = channel();
+
+    //auto select best implementation for specific platform
+    let mut watcher: RecommendedWatcher = try!(Watcher::new(tx));
+
+    //add path to be watched
+    try!(watcher.watch("D:/CleanFolder"));
+    try!(watcher.watch("D:/DirtyFolder"));
+
+    loop {
+        match rx.recv() {
+            Ok(notify::Event{ path: Some(path), op: Ok(op) }) => {
+                println!("{:?} {:?}", op, path);
+            },
+            Err(e) => {
+                println!("watch error {}", e);
+            },
+            _ => ()
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = watch() {
+        println!("Error! {:?}", err)
+    }
 }
